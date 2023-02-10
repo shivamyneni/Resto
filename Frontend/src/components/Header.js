b9a507244d9257c416a21f545ac5a1d9cd86773a
kohlerpop1 <kohlerpop1@gmail.com>
2023-02-09 23:28:27 -0500

    Correct header type and spacing!

--- Frontend/src/components/Header.js
+++ Frontend/src/components/Header.js
@@ -19,7 +19,7 @@ export default function NavBar() {
         <nav className="w-full shadow">
             <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
                 <div>
-                    <div className="flex items-center justify-between py-3 md:py-5 md:block transform transition duration-500 hover:scale-150">
+                    <div className="flex items-center justify-between py-3 md:py-5 md:block transform transition duration-500 hover:scale-125">
                         <Link to="/" onClick={() => { changeTab("") }}>
                             <h2 className="text-2xl font-bold">SimplyBook</h2>
                         </Link>
@@ -27,17 +27,17 @@ export default function NavBar() {
                 </div>
                 <input style={{ height: "2rem" }} className="w-1/2 border-b-2 border-gray" />
                 <div>
-                    <div
-                        className='flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0'
-                    >
+                    <div className='flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0'>
                         <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0 mt-3">
                             <Link to="/signin" className='font-semibold text-md transform transition duration-500 hover:scale-110'
                                 style={{ color: activeTab === "signin" ? "red" : "black" }}
                                 onClick={() => { changeTab("signin") }}>Login</Link>
                             <Link to="/signup" className='font-semibold text-md transform transition duration-500 hover:scale-110'
-                                    style={{ color: activeTab === "signup" ? "red" : "black" }}
-                                    onClick={() => { changeTab("signin") }}>SignUp</Link>
-                            <Link to="/contact" className='font-semibold text-lg transform transition duration-500 hover:scale-110' style={{ color: activeTab === "contact" ? "red" : "black" }} onClick={() => { changeTab("contact") }}>Contact</Link>
+                                style={{ color: activeTab === "signup" ? "red" : "black" }}
+                                onClick={() => { changeTab("signup") }}>SignUp</Link>
+                            <Link to="/contact" className='font-semibold text-lg transform transition duration-500 hover:scale-110'
+                                style={{ color: activeTab === "contact" ? "red" : "black" }}
+                                onClick={() => { changeTab("contact") }}>Contact</Link>
                         </ul>
                     </div>
                 </div>
