44df9c678faf5c8ec73dca17c4ea0d7cffca7d53
kohlerpop1 <kohlerpop1@gmail.com>
2023-02-14 21:25:16 -0500

    Working on sign up page. Its going ok but having trouble with styling!

--- Frontend/src/pages/SignUp.js
+++ Frontend/src/pages/SignUp.js
@@ -1,11 +1,49 @@
-import React from 'react'
+import React from "react";
+import TextField from "@mui/material/TextField";
+import Button from '@mui/material/Button';
 import Header from '../components/Header';
 
 export default function SignUp() {
     return (
         <div style={{ height: "100%" }}>
             <Header />
-            <div>SignUp</div>
+            <div>Sign Up</div>
+            <div className="w-25 items-center justify-between mx-auto">
+
+                <form>
+                    <TextField
+                        type="text"
+                        name="username"
+                        floatingLabelText="user name"
+                    />
+                    <TextField
+                        type="text"
+                        name="email"
+                        floatingLabelText="email"
+                    />
+                    <TextField
+                        type="password"
+                        name="password"
+                        floatingLabelText="password"
+                    />
+                    <TextField
+                        type="password"
+                        name="pwconfirm"
+                        floatingLabelText="confirm password"
+                    />
+                    <br />
+                    <Button
+                        className="w-50"
+                        primary={true}
+                        type="submit"
+                        label="submit"
+                    />
+                </form>
+                <p>
+                    Aleady have an account? <br />
+                    <a href="/">Log in here</a>
+                </p>
+            </div>
         </div>
-    )
-}
+    );
+};
