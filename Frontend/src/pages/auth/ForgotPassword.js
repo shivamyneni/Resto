727952385b31e8250049398b58cd606fbed66b69
AnilMandavilli594 <61509925+AnilMandavilli594@users.noreply.github.com>
2023-03-21 15:04:06 -0400

    ui-forgotPassword

--- Frontend/src/pages/auth/ForgotPassword.js
+++ Frontend/src/pages/auth/ForgotPassword.js
@@ -117,7 +117,7 @@ const ResetPasswordPage = () => {
             <div>
               <button
                 type="submit"
-                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2"
               >
                 Reset Password
               </button>
