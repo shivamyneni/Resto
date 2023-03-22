9a27fa222deeafe5f41666bfcf80f899a2ac7a86
Anshumaan Shankar <shankaa@iu.edu>
2023-03-22 18:46:31 -0400

    Added user rating option

--- Frontend/src/pages/BookSlot.js
+++ Frontend/src/pages/BookSlot.js
@@ -1,65 +1,3 @@
-// import React, { useState } from 'react';
-// import { useParams, useNavigate } from 'react-router-dom';
-// import axios from 'axios';
-
-// export default function BookSlot() {
-//   const { id } = useParams();
-//   const navigate = useNavigate();
-//   const [selectedTime, setSelectedTime] = useState(0);
-//   const [textInput, setTextInput] = useState('');
-
-//   const handleTimeChange = (event) => {
-//     setSelectedTime(parseInt(event.target.value));
-//   };
-
-//   const handleTextChange = (event) => {
-//     setTextInput(event.target.value);
-//   };
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     axios.post("/bookslot",{
-//         time: selectedTime,
-//         text: textInput,
-//         venueId: id,
-//     }).then(res => {
-//         console.log(res)
-//         if (res.data.error){
-//             alert(res.data.error)
-//         } else {
-//             navigate(`/previouspage`)
-//         }
-//     })
-//     .catch((error) => {
-//         const errorCode = error.code;
-//         const errorMessage = error.message;
-//         console.log(errorCode, errorMessage);
-//     });
-//   };
-
-//   return (
-//     <div>
-//       <h1>Book Slot</h1>
-//       <form onSubmit={handleSubmit}>
-//         <label for="time">Time:</label>
-//         <select id="time" value={selectedTime} onChange={handleTimeChange}>
-//           {[...Array(24)].map((_, index) => (
-//             <option value={index} key={index}>
-//               {index}:00 - {index + 1}:00
-//             </option>
-//           ))}
-//         </select>
-//         <br />
-//         <label for="text">Text:</label>
-//         <input type="text" id="text" value={textInput} onChange={handleTextChange} />
-//         <br />
-//         <button type="submit">Confirm Booking</button>
-//       </form>
-//     </div>
-//   );
-// }
-
-
 import React, { useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
@@ -68,35 +6,36 @@ export default function BookSlot() {
   const { id } = useParams();
   const navigate = useNavigate();
   const [selectedTime, setSelectedTime] = useState(0);
-  const [textInput, setTextInput] = useState('');
+  const [court, setCourt] = useState('');
 
   const handleTimeChange = (event) => {
     setSelectedTime(parseInt(event.target.value));
   };
 
-  const handleTextChange = (event) => {
-    setTextInput(event.target.value);
+  const handleCourtChange = (event) => {
+    setCourt(event.target.value);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    axios.post("/bookslot",{
-        time: selectedTime,
-        text: textInput,
-        venueId: id,
-    }).then(res => {
-        console.log(res)
-        if (res.data.error){
-            alert(res.data.error)
-        } else {
-            navigate(`/previouspage`)
-        }
+    axios.post('/bookslot', {
+      time: selectedTime,
+      court,
+      venueId: id,
     })
-    .catch((error) => {
+      .then((res) => {
+        console.log(res);
+        if (res.data.error) {
+          alert(res.data.error);
+        } else {
+          navigate("/VenueDetailsCustomer")
+        }
+      })
+      .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
         console.log(errorCode, errorMessage);
-    });
+      });
   };
 
   return (
@@ -105,7 +44,12 @@ export default function BookSlot() {
       <form onSubmit={handleSubmit} className="w-2/3 flex flex-col items-center">
         <div className="mb-6">
           <label htmlFor="time" className="block text-gray-700 font-bold mb-2">Time:</label>
-          <select id="time" value={selectedTime} onChange={handleTimeChange} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+          <select
+            id="time"
+            value={selectedTime}
+            onChange={handleTimeChange}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          >
             {[...Array(24)].map((_, index) => (
               <option value={index} key={index}>
                 {index}:00 - {index + 1}:00
@@ -114,11 +58,20 @@ export default function BookSlot() {
           </select>
         </div>
         <div className="mb-6">
-          <label htmlFor="text" className="block text-gray-700 font-bold mb-2">Court:</label>
-          <input type="text" id="text" value={textInput} onChange={handleTextChange} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+          <label htmlFor="court" className="block text-gray-700 font-bold mb-2">Court:</label>
+          <input
+            type="text"
+            id="court"
+            value={court}
+            onChange={handleCourtChange}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
         </div>
         <div className="mb-6">
-          <button type="submit" className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded">Confirm Booking</button>
+          <button
+            type="submit"
+            className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded">Confirm Booking
+          </button>
         </div>
       </form>
     </div>
