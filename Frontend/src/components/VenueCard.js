f12546371586dcdb1d4821c0cd133b7099c2baa9
AnilMandavilli594 <61509925+AnilMandavilli594@users.noreply.github.com>
2023-04-08 02:39:41 -0400

    delete-features-for-owner

--- Frontend/src/components/VenueCard.js
+++ Frontend/src/components/VenueCard.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Card, CardContent, Typography } from '@material-ui/core';
+import axios from 'axios';
 
 export default function VenueCard(props) {
     const name = props.name;
@@ -8,8 +9,9 @@ export default function VenueCard(props) {
     const sports = props?.sports?.join(', ');
     const timeslots = props.timeslots == undefined ? [] : props.timeslots.join(', ');
 
+
     return (
-        <Card className='m-1.5 hover:cursor-pointer' onClick={props.onClick} >
+        <Card className='m-1.5 hover:cursor-pointer flex flex-col w-full' onClick={props.onClick}>
             <CardContent>
                 <Typography>Name: {name}</Typography>
                 <Typography>Description: {description}</Typography>
@@ -20,6 +22,6 @@ export default function VenueCard(props) {
                 <Typography className='mt-1'>Time Slots:</Typography>
                 <Typography>{timeslots}</Typography>
             </CardContent>
-        </Card>
+</Card>
     );
 }
