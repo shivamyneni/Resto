85ee3827502cc9dda828ab8a9bfea9a4af65f6ed
Anshumaan Shankar <shankaa@iu.edu>
2023-04-09 19:31:10 -0400

    changing book slot button color - to be fixed.

--- Frontend/src/components/ActivityCard.js
+++ Frontend/src/components/ActivityCard.js
@@ -61,7 +61,7 @@ export default function ActivityCard(props) {
                 ))}
               </div>
               <Button
-                className='bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded mt-2'
+                className='bg-purple-600 hover:bg-purple-700 text-black font-bold py-2 px-4 rounded'
                 onClick={(e) =>
                   navigate(
                     `/uservenues/${venueId}/useractivities/bookslot/${id}`
